12e18d535314929f57a80061db49559557e3b867
Prashant Chaudhary <114799837+prashantkc09@users.noreply.github.com>
2024-02-07 22:46:16 +0530

    Javascript-data types and objects

---
+++ script.js
@@ -0,0 +1,48 @@
+/*console.log("Code is running...")
+console.log("code is also running..")
+console.log("code is looking like a wow....")
+
+var a=prompt("Enter you number")
+var isTrue =prompt("Are you sure you want to leave this page and blast your computer")
+
+if(isTrue){
+    console.log("Computer is blasting")
+}
+else{
+    console.logg("Computer is not blasting")
+}
+console.log("Your number is "+a)*/
+
+//data types&objects
+let a=5
+let b=6;
+let c="Harry";
+let _a="shubham";
+
+//console.log(a+b+8);
+//console.log(typeof a,typeof b,typeof c);
+{
+    let a =66; 
+    console.log(a)
+}
+
+//const a1=6; invalid
+
+let x="Harry bhai";
+let y=22;
+let z=3.55;
+const p=true; 
+let q=undefined;
+let r =null;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let o ={
+    "name": "Harry",
+    "job code": 5600
+}
+
+console.log(o);
+o.salary =  "100crores"
+console.log(o );
