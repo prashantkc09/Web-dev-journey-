95fdd49a3db5bd89f441aeccb79de723a527afce
Prashant Chaudhary <114799837+prashantkc09@users.noreply.github.com>
2024-06-22 23:40:06 +0530

    Add files via upload

---
+++ challenge_1.java
@@ -0,0 +1,7 @@
+public class challenge_1 {
+    public static void main(String[]args){
+        System.out.println("*\n* *\n* * *\n* * * *\n* * * * *");
+        //printing the pattern using same print statement
+
+    }
+}
